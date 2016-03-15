mgencur/a4m36jee-2014-infinispan-seminar

[infinispan-seminar] Initial version of infinispan seminar

src/main/java/cz/ctu/fee/a4m36jee/seminar/infinispan/model/Car.java
@@ -1,0 +1,109 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.infinispan.model;
+
+import java.io.Serializable;
+
+/**
+ * Represents a car in the car mart. Car objects are stored in the cache.
+ *
+ * @author Martin Gencur
+ */
+public class Car implements Serializable {
+
+    private static final long serialVersionUID = 188164481825309731L;
+
+    public enum CarType {
+        SEDAN, HATCHBACK, COMBI, CABRIO, ROADSTER
+    }
+
+    public enum Country {
+        CZECH_REPUBLIC, USA, GERMANY
+    }
+
+    public Car() {
+    }
+
+    public Car(String brand, double displacement, CarType type, String color, String numberPlate, Country country) {
+        this.brand = brand;
+        this.displacement = displacement;
+        this.type = type;
+        this.color = color;
+        this.numberPlate = numberPlate;
+        this.country = country;
+    }
+
+    private String brand;
+    private double displacement;
+    private CarType type;
+    private String color;
+    private String numberPlate;
+    private Country country;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public CarType getType() {
+        return type;
+    }
+
+    public void setType(CarType type) {
+        this.type = type;
+    }
+
+    public String getNumberPlate() {
+        return numberPlate;
+    }
+
+    public void setNumberPlate(String numberPlate) {
+        this.numberPlate = numberPlate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(double displacement) {
+        this.displacement = displacement;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/infinispan/session/CacheContainerProvider.java
@@ -1,0 +1,70 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.infinispan.session;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
+
+import org.infinispan.commons.api.BasicCacheContainer;
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfiguration;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
+
+/**
+ * Implementation of {@link CacheContainerProvider} creating a programmatically configured DefaultCacheManager.
+ * Libraries of Infinispan need to be bundled with the application - this is called "library" mode.
+ *
+ * @author Martin Gencur
+ */
+@ApplicationScoped
+public class CacheContainerProvider {
+
+    private Logger log = Logger.getLogger(this.getClass().getName());
+
+    private BasicCacheContainer manager;
+
+    public BasicCacheContainer getCacheContainer() {
+        if (manager == null) {
+
+            //*************** amend the configuration ***************
+
+            GlobalConfiguration glob = new GlobalConfigurationBuilder().build();
+            Configuration loc = new ConfigurationBuilder().build();
+
+            //*************** amend the configuration ***************
+
+            manager = new DefaultCacheManager(glob, loc, true); //true means start the cache manager immediately
+            log.info("=== Using DefaultCacheManager (library mode) ===");
+        }
+        return manager;
+    }
+
+    @PreDestroy
+    public void cleanUp() {
+        manager.stop();
+        manager = null;
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/infinispan/session/CarTypeManager.java
@@ -1,0 +1,41 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.infinispan.session;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+
+import cz.ctu.fee.a4m36jee.seminar.infinispan.model.Car.CarType;
+
+/**
+ * Produces an array of supported car types.
+ *
+ * @author Martin Gencur
+ */
+public class CarTypeManager {
+
+    @Produces
+    @Named
+    public CarType[] getCarTypes() {
+        return CarType.values();
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/infinispan/session/CountryManager.java
@@ -1,0 +1,41 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.infinispan.session;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+
+import cz.ctu.fee.a4m36jee.seminar.infinispan.model.Car.Country;
+
+/**
+ * Produces an array of supported countries
+ *
+ * @author Martin Gencur
+ */
+public class CountryManager {
+
+    @Produces
+    @Named
+    public Country[] getCountries() {
+        return Country.values();
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/infinispan/session/LocalStatisticsProvider.java
@@ -1,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.infinispan.session;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named("stats")
+@RequestScoped
+public class LocalStatisticsProvider implements StatisticsProvider {
+
+    public String getModifications() {
+        return String.valueOf(-1);
+    }
+
+    public String getCreations() {
+        return String.valueOf(-1);
+    }
+
+    public String getRemovals() {
+        return String.valueOf(-1);
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/infinispan/session/StatisticsProvider.java
@@ -1,0 +1,36 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.ctu.fee.a4m36jee.seminar.infinispan.session;
+
+/**
+ * Provides cache statistics.
+ *
+ * @author Martin Gencur
+ */
+public interface StatisticsProvider {
+
+    public String getModifications();
+
+    public String getCreations();
+
+    public String getRemovals();
+}

mcimbora/pv243-2015-infinispan-seminar

src/main/java/cz/muni/fi/pv243/seminar/infinispan/model/Car.java
@@ -1,0 +1,109 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.infinispan.model;
+
+import java.io.Serializable;
+
+/**
+ * Represents a car in the car mart. Car objects are stored in the cache.
+ *
+ * @author Martin Gencur
+ */
+public class Car implements Serializable {
+
+    private static final long serialVersionUID = 188164481825309731L;
+
+    public enum CarType {
+        SEDAN, HATCHBACK, COMBI, CABRIO, ROADSTER
+    }
+
+    public enum Country {
+        CZECH_REPUBLIC, USA, GERMANY
+    }
+
+    public Car() {
+    }
+
+    public Car(String brand, double displacement, CarType type, String color, String numberPlate, Country country) {
+        this.brand = brand;
+        this.displacement = displacement;
+        this.type = type;
+        this.color = color;
+        this.numberPlate = numberPlate;
+        this.country = country;
+    }
+
+    private String brand;
+    private double displacement;
+    private CarType type;
+    private String color;
+    private String numberPlate;
+    private Country country;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public CarType getType() {
+        return type;
+    }
+
+    public void setType(CarType type) {
+        this.type = type;
+    }
+
+    public String getNumberPlate() {
+        return numberPlate;
+    }
+
+    public void setNumberPlate(String numberPlate) {
+        this.numberPlate = numberPlate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(double displacement) {
+        this.displacement = displacement;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/infinispan/session/CacheContainerProvider.java
@@ -1,0 +1,70 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.infinispan.session;
+
+import java.util.logging.Logger;
+
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
+
+import org.infinispan.commons.api.BasicCacheContainer;
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfiguration;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
+
+/**
+ * Implementation of {@link CacheContainerProvider} creating a programmatically configured DefaultCacheManager.
+ * Libraries of Infinispan need to be bundled with the application - this is called "library" mode.
+ *
+ * @author Martin Gencur
+ */
+@ApplicationScoped
+public class CacheContainerProvider {
+
+    private Logger log = Logger.getLogger(this.getClass().getName());
+
+    private BasicCacheContainer manager;
+
+    public BasicCacheContainer getCacheContainer() {
+        if (manager == null) {
+
+            //*************** amend the configuration ***************
+
+            GlobalConfiguration glob = new GlobalConfigurationBuilder().build();
+            Configuration loc = new ConfigurationBuilder().build();
+
+            //*************** amend the configuration ***************
+
+            manager = new DefaultCacheManager(glob, loc, true); //true means start the cache manager immediately
+            log.info("=== Using DefaultCacheManager (library mode) ===");
+        }
+        return manager;
+    }
+
+    @PreDestroy
+    public void cleanUp() {
+        manager.stop();
+        manager = null;
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/infinispan/session/CarTypeManager.java
@@ -1,0 +1,41 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.infinispan.session;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+
+import cz.muni.fi.pv243.seminar.infinispan.model.Car.CarType;
+
+/**
+ * Produces an array of supported car types.
+ *
+ * @author Martin Gencur
+ */
+public class CarTypeManager {
+
+    @Produces
+    @Named
+    public CarType[] getCarTypes() {
+        return CarType.values();
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/infinispan/session/CountryManager.java
@@ -1,0 +1,41 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.infinispan.session;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+
+import cz.muni.fi.pv243.seminar.infinispan.model.Car.Country;
+
+/**
+ * Produces an array of supported countries
+ *
+ * @author Martin Gencur
+ */
+public class CountryManager {
+
+    @Produces
+    @Named
+    public Country[] getCountries() {
+        return Country.values();
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/infinispan/session/LocalStatisticsProvider.java
@@ -1,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.infinispan.session;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named("stats")
+@RequestScoped
+public class LocalStatisticsProvider implements StatisticsProvider {
+
+    public String getModifications() {
+        return String.valueOf(-1);
+    }
+
+    public String getCreations() {
+        return String.valueOf(-1);
+    }
+
+    public String getRemovals() {
+        return String.valueOf(-1);
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/infinispan/session/StatisticsProvider.java
@@ -1,0 +1,36 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package cz.muni.fi.pv243.seminar.infinispan.session;
+
+/**
+ * Provides cache statistics.
+ *
+ * @author Martin Gencur
+ */
+public interface StatisticsProvider {
+
+    public String getModifications();
+
+    public String getCreations();
+
+    public String getRemovals();
+}